7114fb29c7bd83dea14bfd23766412a092f16283
Charly Riviere <charly.riviere@gmail.com>
2016-06-23 00:39:21 +0200

    index page initialization

---
+++ src/AppBundle/Controller/IndexController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: charly
+ * Date: 22/06/2016
+ * Time: 21:29
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class IndexController extends Controller
+{
+
+    /**
+     * @Route("/index")
+     */
+    public function numberAction()
+    {
+        return $this->render(
+            'index.html.twig'
+        );
+    }
+
+}
